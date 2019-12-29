1f153301e296e051934a2f4961e231839ac7cd28
Cagri <cagri.yonca@ceng.ktu.edu.tr>
2019-12-29 20:39:55 +0300

    first

--- BeeAlgorithm.py
+++ BeeAlgorithm.py
@@ -1,12 +1,20 @@
 import numpy as np
-import random
+
+def read_values_from_file(filename):
+    f = open(filename, "r")
+    array = []
+    for line in f:
+        array.append(float(line))
+    f.close()
+
+    return array
 
 class Bee_Algorithm():
     def __init__(self, nPioneerBees, nFollowerBees,
                  cap_filename, req_filename, dist_filename):
-        self.capacities = self.read_values_from_file(cap_filename)
-        self.demands = self.read_values_from_file(req_filename)
-        self.distances = self.read_values_from_file(dist_filename)
+        self.capacities = read_values_from_file(cap_filename)
+        self.demands = read_values_from_file(req_filename)
+        self.distances = read_values_from_file(dist_filename)
         self.num_of_stores = len(self.capacities)
         self.num_of_customers = len(self.demands)
 
@@ -24,15 +32,8 @@ class Bee_Algorithm():
                                     self.distances))
         return array
 
-    def read_values_from_file(self, filename):
-        with open(filename) as f:
-            array = []
-            for line in f:
-                array.append(float(line))
-            return array
-
     def print_matrix(self, index):
-        print(self.PioneerBees[index].get_matrix())
+        print(self.PioneerBees[index].get_matrix()[0])
 
 
 class PioneerBee():
@@ -51,23 +52,13 @@ class PioneerBee():
         return self.pioneer_bees
 
     def random_matrix(self):
-        print("random_matrix {}".format(self.capacities2[0]))
-
-        with open("C:/Users/user/Desktop/CerenProje/Müşteri-Talep.txt") as f:
-            array = []
-            for line in f:
-                array.append(float(line))
-            return array
+        self.capacities = read_values_from_file("Depo-Kapasite.txt")
 
         for customer in range(self.num_of_customers):
             while (self.pioneer_bees[customer].sum() == 0):
                 seed = np.random.randint(self.num_of_stores)
-                if (self.capacities2[seed] >= self.demands[customer]):
+                if (self.capacities[seed] >= self.demands[customer]):
                     self.pioneer_bees[customer, seed] = 1
-                    self.capacities2[seed] -= self.demands[customer]
-        print("CIKMIYOKİ {}".format(self.capacities2[0]))
+                    self.capacities[seed] -= self.demands[customer]
 
-a = Bee_Algorithm(10, 0,
-                  "C:/Users/user/Desktop/CerenProje/Depo-Kapasite.txt",
-                  "C:/Users/user/Desktop/CerenProje/Müşteri-Talep.txt",
-                  "C:/Users/user/Desktop/CerenProje/Uzaklık-km.txt")
+a = Bee_Algorithm(10, 0,"Depo-Kapasite.txt","Müşteri-Talep.txt","Uzaklık-km.txt")
